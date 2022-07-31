6dfacd93f326bac8fec0f6744a17a9e726fe7351
ujvarivera <ujvariveronika0320@gmail.com>
2022-07-31 13:00:43 +0200

    datatables from users

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,6 +9,6 @@ class UserController extends Controller
 {
     public function index()
     {
-        return User::all();
+        return view('users.index');
     }
 }
---
+++ app/Http/Livewire/UsersTable.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use App\Models\User;
+use Mediconesystems\LivewireDatatables\Column;
+use Mediconesystems\LivewireDatatables\DateColumn;
+use Mediconesystems\LivewireDatatables\NumberColumn;
+use Mediconesystems\LivewireDatatables\Http\Livewire\LivewireDatatable;
+
+class UsersTable extends LivewireDatatable
+{
+    public $model = User::class;
+    public $exportable = true;
+    public $complex = true;
+
+    public function columns()
+    {
+        return [
+            
+            NumberColumn::name('id')
+            ->label('ID'),
+
+            Column::name('name')
+            ->defaultSort('asc')
+            ->searchable()
+            ->hideable()
+            ->filterable(),
+            
+            Column::name('email')
+                ->label('email')
+                ->group('group1')
+                ->searchable()
+                ->hideable()
+                ->filterable(),
+
+            DateColumn::name('email_verified_at')
+                ->label('verified_at')
+                ->filterable()
+
+            
+
+        ];
+    }
+
+}
